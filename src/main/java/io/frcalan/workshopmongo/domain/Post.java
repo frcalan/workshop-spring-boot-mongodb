b808a856a0e26b0cfef0480bb5d1aa851574e09a
Alan Silva <frcalan@gmail.com>
2023-04-11 20:38:15 -0300

    Acrescentando comentários aos posts

--- src/main/java/io/frcalan/workshopmongo/domain/Post.java
+++ src/main/java/io/frcalan/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package io.frcalan.workshopmongo.domain;
 
 import io.frcalan.workshopmongo.dto.AuthorDTO;
+import io.frcalan.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,9 +21,8 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-
     private AuthorDTO author;
-
+    private List<CommentDTO> comments = new ArrayList<>();
     public Post() {
     }
 
@@ -72,6 +74,13 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
