59751c0d5cb9922b373afdc49c3fcbbe716a6928
Alan Silva <frcalan@gmail.com>
2023-04-11 21:42:14 -0300

    Consulta simples com query methods

--- src/main/java/io/frcalan/workshopmongo/resources/PostResource.java
+++ src/main/java/io/frcalan/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package io.frcalan.workshopmongo.resources;
 import io.frcalan.workshopmongo.domain.Post;
 import io.frcalan.workshopmongo.domain.User;
 import io.frcalan.workshopmongo.dto.UserDTO;
+import io.frcalan.workshopmongo.resources.util.URL;
 import io.frcalan.workshopmongo.service.PostService;
 import io.frcalan.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,6 +27,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/io/frcalan/workshopmongo/service/PostService.java
+++ src/main/java/io/frcalan/workshopmongo/service/PostService.java
@@ -23,4 +23,8 @@ public class PostService {
         return post.orElseThrow(() -> new ObjectNotFoundException("Post not found."));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
+
 }
